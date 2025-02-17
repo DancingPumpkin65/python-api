3ff03191e096fc01d5e286a3422aa960e790fac1
Scripted Logic <ouassbader184@gmail.com>
2025-02-17 11:59:03 +0100

    Setting FastAPI with some basic definitions

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
-from flask import Flask, request, jsonify
+# Fast API advantages:
+#     -Data validation: Enforces types in function signatures, and data schemas using Pydantic. 
+#    Perform data validation, serialization, and documentation.
+#     -Auto documentaion: Swagger UI / ReDoc automatically generated and API documentation updated.
+#     -Auto completion and code suggestions: PyCharm, Visual Studio Code, and other editors 
+#    support FastAPI.
 
-app = Flask(__name__)
+# Baic Flask API
+# from flask import Flask, request, jsonify
 
-@app.route('/get-user/<user_id>')
-def get_user(user_id):
-    user_data = {
-          'user_id': user_id,
-            'name': 'John Doe',
-            'email': 'john.doe@example.com'
-    }
+# app = Flask(__name__)
 
-    extra = request.args.get('extra')
-    if extra:
-        user_data['extra'] = extra
+# @app.route('/get-user/<user_id>')
+# def get_user(user_id):
+#     user_data = {
+#           'user_id': user_id,
+#             'name': 'John Doe',
+#             'email': 'john.doe@example.com'
+#     }
+
+#     extra = request.args.get('extra')
+#     if extra:
+#         user_data['extra'] = extra
     
-    return jsonify(user_data), 200
+#     return jsonify(user_data), 200
 
-if __name__ == '__main__':
-        app.run(debug=True)
+# if __name__ == '__main__':
+#         app.run(debug=True)
+
+# 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Endpoint: the point of entry in a communication channel when two systems are interacting.
+# For example: '/hello' in localhost:8000/hello
+
